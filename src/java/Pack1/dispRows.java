2e20c59dfcb26a82863cc18f468fd399108b7366
pavitrakumar78 <pavitrakumar78@gmail.com>
2015-05-25 16:38:30 +0530

    Update dispRows.java

--- src/java/Pack1/dispRows.java
+++ src/java/Pack1/dispRows.java
@@ -57,22 +57,18 @@ public class dispRows extends HttpServlet {
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      //  String page = request.getSession().getAttribute("HOME").toString();
-        // if(page!=null && page.contains("home")){
-        //     //if it is a homepage, then redirect it!
         response.sendRedirect("SearchResults.jsp");
-       // }
 
         response.setContentType("text/html");
         System.out.println(request.getContextPath());
 
         keyword = request.getParameter("keyword");
-        System.out.println("!-!-!->" + keyword);
+        //System.out.println("----->" + keyword);
         String JDBC_DRIVER = "com.mysql.jdbc.Driver";
         String DB_URL = "jdbc:mysql://localhost/jproject";
 
         String USER = "root";
-        String PASS = "pavitrakumar";
+        String PASS = "password";
 
         Statement stmt = null;
         Connection conn = null;
@@ -118,26 +114,3 @@ public class dispRows extends HttpServlet {
         }
     }
 }
-
-/**
- * Handles the HTTP <code>POST</code> method.
- *
- * @param request servlet request
- * @param response servlet response
- * @throws ServletException if a servlet-specific error occurs
- * @throws IOException if an I/O error occurs
- */
-//@Override
-// protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//       throws ServletException, IOException {
-//    processRequest(request, response);
-// }
-/**
- * Returns a short description of the servlet.
- *
- * @return a String containing servlet description
- */
-   // @Override
-// public String getServletInfo() {
-//  return "Short description";
-// }// </editor-fold>
